e08f8c66541f1ff9b798af6c3b2b1f21b0a6b372
jhonnyscerni <jhonnyscerni@gmail.com>
2024-07-14 19:38:14 -0300

    build gitactions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlbase: 'http://ec2-54-175-65-66.compute-1.amazonaws.com',
+  urlbase: 'https://political-management.onrender.com/',
   imagensUrl: 'http://ec2-54-175-65-66.compute-1.amazonaws.com'
 };
